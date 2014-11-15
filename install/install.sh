65c5e7a36e3b825d514ae9e01a2ab7f7030ae3e6
Ben Wunschel <shadow_ronin@gmx.com>
2014-11-15 11:07:52 -0500

    updated install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,24 @@
 #! /bin/bash
 
+function apache-config {
+  cp apache2.conf /etc/apache2/apache2.conf
+  sed "s|IRONCLOUDDES|$install_location/IronCloud/public_html|g" /etc/apache2/apache2.conf
+}
+
+function IronCloud-config {
+  echo "<?php" > $install_location"/private_html/includes/psl-config.php"
+  echo "define("HOST", "localhost");" >> $install_location"/private_html/includes/psl-config.php"
+  echo "define("USER", "loginuser");" >> $install_location"/private_html/includes/psl-config.php"
+  echo "define("PASSWORD", "examplepassword");" >> $install_location"/private_html/includes/psl-config.php"
+  echo "define("DATABASE", "login");" >> $install_location"/private_html/includes/psl-config.php"
+  echo  "" >> $install_location"/private_html/includes/psl-config.php"
+  echo "define("CAN_REGISTER", "any");" >> $install_location"/private_html/includes/psl-config.php"
+  echo "define("DEFAULT_ROLE", "member");" >> $install_location"/private_html/includes/psl-config.php"
+  echo "" >> $install_location"/private_html/includes/psl-config.php"
+  echo "define("SECURE", FALSE);" >> $install_location"/private_html/includes/psl-config.php"
+
+}
+
 install_location='/var/www'
 mysql_database_name="IronCloud"
 mysql_admin_user="root"
@@ -22,13 +41,13 @@ service apache2 restart
 echo ""
 
 echo "Decompressing package"
-tar -xfv IronCoud.tar -C $install_location
+tar -xfv IronCloud.tar -C $install_location
 
 echo "Setting up mysql"
 #configure apache & make mysql tables
-temp = "CREATE DATABASE $mysql_database_name;"
+temp="CREATE DATABASE $mysql_database_name;"
 mysql -u $mysql_admin_user -p $mysql_admin_pass -e $temp
-temp = "CREATE TABLE \`members\` (\`id\` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \`username\` VARCHAR(30) NOT NULL, \`email\` VARCHAR(50) NOT NULL,\`password\` CHAR(128) NOT NULL,\`salt\` CHAR(128) NOT NULL) ENGINE = InnoDB;"
+temp="CREATE TABLE \`members\` (\`id\` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \`username\` VARCHAR(30) NOT NULL, \`email\` VARCHAR(50) NOT NULL,\`password\` CHAR(128) NOT NULL,\`salt\` CHAR(128) NOT NULL) ENGINE = InnoDB;"
 mysql -u $mysql_admin_user -p $mysql_admin_pass -D $mysql_database_name -e $temp
 
 echo "Configureing server"
@@ -36,22 +55,3 @@ apache-config
 IronCloud-config
 
 echo "Cleaning up"
-
-function apache-config {
-  cp apache2.conf /etc/apache2/apache2.conf
-  sed "s|IRONCLOUDDES|$install_location/IronCloud/public_html|g" /etc/apache2/apache2.conf
-}
-
-function IronCloud-config {
-  echo "<?php" > $install_location"/private_html/includes/psl-config.php"
-  echo "define("HOST", "localhost");" >> $install_location"/private_html/includes/psl-config.php"
-  echo "define("USER", "loginuser");" >> $install_location"/private_html/includes/psl-config.php"
-  echo "define("PASSWORD", "examplepassword");" >> $install_location"/private_html/includes/psl-config.php"
-  echo "define("DATABASE", "login");" >> $install_location"/private_html/includes/psl-config.php"
-  echo  "" >> $install_location"/private_html/includes/psl-config.php"
-  echo "define("CAN_REGISTER", "any");" >> $install_location"/private_html/includes/psl-config.php"
-  echo "define("DEFAULT_ROLE", "member");" >> $install_location"/private_html/includes/psl-config.php"
-  echo "" >> $install_location"/private_html/includes/psl-config.php"
-  echo "define("SECURE", FALSE);" >> $install_location"/private_html/includes/psl-config.php"
-
-}

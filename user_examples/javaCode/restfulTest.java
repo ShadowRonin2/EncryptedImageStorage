7c36f2578580b0ab23b7c19a22dfc85a9cf50cfa
Ben Wunschel <shadow_ronin@gmx.com>
2014-11-19 15:05:01 -0500

    bug fix

--- user_examples/javaCode/restfulTest.java
+++ user_examples/javaCode/restfulTest.java
@@ -1,5 +1,10 @@
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -17,8 +22,12 @@ import org.apache.http.util.EntityUtils;
 public class restfulTest {
 	
 	public static void main(String[] args) throws Exception{
-		hello();
+		//hello();
 		postFile();
+		getFile();
+		//delF();
+		//putPassword();
+		
 	}
 	
 	public static void hello() throws Exception {
@@ -27,7 +36,7 @@ public class restfulTest {
 	    String ip = "127.0.0.1:8080";
 	    
 	    //login
-	    HttpPost loginPost = new HttpPost("http://" + ip + "/restful/get/hello.json");
+	    HttpPost loginPost = new HttpPost("http://" + ip + "/get/hello.json");
 	    MultipartEntityBuilder loginBuilder = MultipartEntityBuilder.create();
 	    loginBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
 	    ContentBody user = new StringBody("foo");
@@ -47,13 +56,42 @@ public class restfulTest {
 	    }
 	    httpclient.close();
 	}
+	
+	public static void delF() throws Exception {
+	    CloseableHttpClient httpclient = HttpClients.createDefault();
+	    //change ip to what your host ip is
+	    String ip = "127.0.0.1:8080";
+	    
+	    //login
+	    HttpPost loginPost = new HttpPost("http://" + ip + "/restful/delete/file.json");
+	    MultipartEntityBuilder loginBuilder = MultipartEntityBuilder.create();
+	    loginBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+	    ContentBody user = new StringBody("foo");
+	    ContentBody pass = new StringBody("bar");
+	    ContentBody file = new StringBody("sample.jpeg");
+	    
+	    loginBuilder.addPart("username", user);
+	    loginBuilder.addPart("password", pass);
+	    loginBuilder.addPart("filename", file);
+	    HttpEntity lgEn = loginBuilder.build();
+	    loginPost.setEntity(lgEn);
+	    
+	    System.out.println("executing request " + loginPost.getRequestLine());
+	    HttpResponse response2 = httpclient.execute(loginPost);
+	    HttpEntity reEntity = response2.getEntity();
+	    System.out.println(response2.getStatusLine());
+	    if (reEntity != null) {
+	      System.out.println(EntityUtils.toString(reEntity));
+	    }
+	    httpclient.close();
+	}
 	 
 	public static void postFile() throws Exception{
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 	    //change ip to what your host ip is
 	    String ip = "127.0.0.1:8080";
 	    //upload file
-	    HttpPost httppost = new HttpPost("http://"+ip+"/post/file.xml");
+	    HttpPost httppost = new HttpPost("http://"+ip+"/put/file.xml");
 	    File file = new File("sample.jpeg");
 	    
 	    MultipartEntityBuilder builder = MultipartEntityBuilder.create();        
@@ -76,4 +114,78 @@ public class restfulTest {
 	    
 	    httpclient.close();
 	  }
+	
+	public static void getFile() throws Exception {
+	    CloseableHttpClient httpclient = HttpClients.createDefault();
+	    //change ip to what your host ip is
+	    String ip = "127.0.0.1:8080";
+	    String out = "";
+	    
+	    //login
+	    HttpPost loginPost = new HttpPost("http://" + ip + "/get/file.xml");
+	    MultipartEntityBuilder loginBuilder = MultipartEntityBuilder.create();
+	    loginBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+	    ContentBody user = new StringBody("foo");
+	    ContentBody pass = new StringBody("bar");
+	    ContentBody file = new StringBody("sample.jpeg");
+	    
+	    loginBuilder.addPart("username", user);
+	    loginBuilder.addPart("password", pass);
+	    loginBuilder.addPart("filename", file);
+	    
+	    HttpEntity lgEn = loginBuilder.build();
+	    loginPost.setEntity(lgEn);
+	    
+	    System.out.println("executing request " + loginPost.getRequestLine());
+	    HttpResponse response2 = httpclient.execute(loginPost);
+	    HttpEntity reEntity = response2.getEntity();
+	    System.out.println(response2.getStatusLine());
+	    if (reEntity != null) {
+	      out = EntityUtils.toString(reEntity);
+	      System.out.println(out);
+	    }
+	  
+	    System.out.println();
+	    Scanner in = new Scanner(out);
+	    in.nextLine();
+	    in.nextLine();
+	    in.nextLine();
+	    String line = in.nextLine();
+	    line = line.substring(7, line.length()-7);
+	    System.out.println(line);
+	    byte[] decoded = Base64.decodeBase64(line);
+	    OutputStream fileOut = new BufferedOutputStream(new FileOutputStream("/home/shadowx/Desktop/test.jpeg"));
+	    fileOut.write(decoded);
+	    
+	    httpclient.close();
 	}
+
+	public static void putPassword() throws Exception {
+	    CloseableHttpClient httpclient = HttpClients.createDefault();
+	    //change ip to what your host ip is
+	    String ip = "127.0.0.1:8080";
+	    
+	    //login
+	    HttpPost loginPost = new HttpPost("http://" + ip + "/restful/put/password.json");
+	    MultipartEntityBuilder loginBuilder = MultipartEntityBuilder.create();
+	    loginBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+	    ContentBody user = new StringBody("foo");
+	    ContentBody pass = new StringBody("bar");
+	    ContentBody newPassword = new StringBody("testytesttest");
+	    
+	    loginBuilder.addPart("username", user);
+	    loginBuilder.addPart("password", pass);
+	    loginBuilder.addPart("newPassword", newPassword);
+	    HttpEntity lgEn = loginBuilder.build();
+	    loginPost.setEntity(lgEn);
+	    
+	    System.out.println("executing request " + loginPost.getRequestLine());
+	    HttpResponse response2 = httpclient.execute(loginPost);
+	    HttpEntity reEntity = response2.getEntity();
+	    System.out.println(response2.getStatusLine());
+	    if (reEntity != null) {
+	      System.out.println(EntityUtils.toString(reEntity));
+	    }
+	    httpclient.close();
+	}
+}
